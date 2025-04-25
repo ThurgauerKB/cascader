1658691dd7d1ed96d5f38b0b946923c41ccc01ee
giotto <giottolino@gmail.com>
2025-04-25 12:45:49 +0200

    feat(e2e): add more test cases to tests last-observed-restart annotation (#36)
    * feat(logging): improve logger setup
    * feat(e2e): add more test cases to tests last-observed-restart annotation
    * Revert "feat(logging): improve logger setup"
    
    This reverts commit 79bd50a24222f5ed03f6fa359e0709e9aa82da24.

--- test/e2e/daemonset_test.go
+++ test/e2e/daemonset_test.go
@@ -88,7 +88,7 @@ var _ = Describe("DaemonSet Workload", Ordered, func() {
 	},
 	)
 
-	It("Restart Pod twice", func(ctx SpecContext) {
+	It("Restart Pod twice", func(ctx SpecContext) { // nolint:dupl
 		obj1Name := testutils.GenerateUniqueName("ds1")
 		obj2Name := testutils.GenerateUniqueName("ds2")
 
@@ -108,6 +108,60 @@ var _ = Describe("DaemonSet Workload", Ordered, func() {
 		obj2ID := testutils.GenerateID(obj2)
 
 		testutils.RestartResource(ctx, obj1)
+
+		By(fmt.Sprintf("Detect restart of %s", obj1ID))
+		testutils.ContainsLogs(fmt.Sprintf(
+			"%q,\"workloadID\":%q",
+			restartDetectedMsg,
+			obj1ID,
+		), 1*time.Minute, 2*time.Second)
+
+		By(fmt.Sprintf("Fetches restart of %s", obj2ID))
+		testutils.ContainsLogs(
+			fmt.Sprintf("%q,\"workloadID\":%q,\"targetID\":%q", successfullTriggerTargetMsg, obj1ID, obj2ID),
+			1*time.Minute,
+			2*time.Second,
+		)
+
+		testutils.LogBuffer.Reset()
+
+		testutils.DeleteRandomPod(ctx, obj1)
+
+		By(fmt.Sprintf("Detect restart of %s", obj1ID))
+		testutils.ContainsLogs(fmt.Sprintf(
+			"%q,\"workloadID\":%q",
+			restartDetectedMsg,
+			obj1ID,
+		), 1*time.Minute, 2*time.Second)
+
+		By(fmt.Sprintf("Fetches restart of %s", obj2ID))
+		testutils.ContainsLogs(
+			fmt.Sprintf("%q,\"workloadID\":%q,\"targetID\":%q", successfullTriggerTargetMsg, obj1ID, obj2ID),
+			1*time.Minute,
+			2*time.Second,
+		)
+	})
+
+	It("Delete Pod twice", func(ctx SpecContext) { // nolint:dupl
+		obj1Name := testutils.GenerateUniqueName("ds1")
+		obj2Name := testutils.GenerateUniqueName("ds2")
+
+		obj1 := testutils.CreateDaemonSet(
+			ctx,
+			ns,
+			obj1Name,
+			testutils.WithAnnotation(daemonSetAnnotation, obj2Name),
+		)
+		obj1ID := testutils.GenerateID(obj1)
+
+		obj2 := testutils.CreateDaemonSet(
+			ctx,
+			ns,
+			obj2Name,
+		)
+		obj2ID := testutils.GenerateID(obj2)
+
+		testutils.DeleteRandomPod(ctx, obj1)
 
 		By(fmt.Sprintf("Detect restart of %s", obj1ID))
 		testutils.ContainsLogs(fmt.Sprintf(
--- test/e2e/deployment_test.go
+++ test/e2e/deployment_test.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/thurgauerkb/cascader/internal/utils"
 	"github.com/thurgauerkb/cascader/test/testutils"
 
 	. "github.com/onsi/ginkgo/v2" // nolint:staticcheck
@@ -86,7 +87,7 @@ var _ = Describe("Deployment workload", Ordered, func() {
 			2*time.Second)
 	})
 
-	It("Delete Pod twice", func(ctx SpecContext) {
+	It("Restart Pod twice", func(ctx SpecContext) { // nolint:dupl
 		obj1Name := testutils.GenerateUniqueName("dep1")
 		obj2Name := testutils.GenerateUniqueName("dep2")
 
@@ -107,6 +108,58 @@ var _ = Describe("Deployment workload", Ordered, func() {
 		obj2ID := testutils.GenerateID(obj2)
 
 		testutils.RestartResource(ctx, obj1)
+
+		By(fmt.Sprintf("Detect restart of %s", obj1ID))
+		testutils.ContainsLogs(
+			fmt.Sprintf("%q,\"workloadID\":%q", restartDetectedMsg, obj1ID),
+			1*time.Minute,
+			2*time.Second)
+
+		By(fmt.Sprintf("Fetches restart of %s", obj2ID))
+		testutils.ContainsLogs(
+			fmt.Sprintf("%q,\"workloadID\":%q,\"targetID\":%q", successfullTriggerTargetMsg, obj1ID, obj2ID),
+			1*time.Minute,
+			2*time.Second)
+
+		testutils.LogBuffer.Reset()
+
+		testutils.DeleteRandomPod(ctx, obj1)
+
+		By(fmt.Sprintf("Detect restart of %s", obj1ID))
+		testutils.ContainsLogs(
+			fmt.Sprintf("%q,\"workloadID\":%q", restartDetectedMsg, obj1ID),
+			1*time.Minute,
+			2*time.Second)
+
+		By(fmt.Sprintf("Fetches restart of %s", obj2ID))
+		testutils.ContainsLogs(
+			fmt.Sprintf("%q,\"workloadID\":%q,\"targetID\":%q", successfullTriggerTargetMsg, obj1ID, obj2ID),
+			1*time.Minute,
+			2*time.Second)
+	})
+
+	It("Delete Pod twice", func(ctx SpecContext) { // nolint:dupl
+		obj1Name := testutils.GenerateUniqueName("dep1")
+		obj2Name := testutils.GenerateUniqueName("dep2")
+
+		obj1 := testutils.CreateDeployment(
+			ctx,
+			ns,
+			obj1Name,
+			testutils.WithAnnotation(deploymentAnnotation, obj2Name),
+			testutils.WithAnnotation(utils.LastObservedRestartKey, time.Now().Format(time.RFC3339)),
+			testutils.WithStartupProbe(5),
+		)
+		obj1ID := testutils.GenerateID(obj1)
+
+		obj2 := testutils.CreateDeployment(
+			ctx,
+			ns,
+			obj2Name,
+		)
+		obj2ID := testutils.GenerateID(obj2)
+
+		testutils.DeleteRandomPod(ctx, obj1)
 
 		By(fmt.Sprintf("Detect restart of %s", obj1ID))
 		testutils.ContainsLogs(
