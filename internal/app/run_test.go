644c9106cc27d7251381285d4ee3de20868c891c
gi8 <giottolino@gmail.com>
2026-07-08 15:21:23 +0200

    fix: unit test

--- internal/app/run_test.go
+++ internal/app/run_test.go
@@ -104,9 +104,9 @@ func TestRun(t *testing.T) {
 		err := Run(ctx, "v0.0.0", args, out, errOut)
 
 		require.Error(t, err)
-		assert.EqualError(t, err, "invalid value for flag --log-encoder: must be one of: json, console")
+		assert.EqualError(t, err, "invalid value for flag --log-encoder: \"invalid\" must be one of: json, console")
 		assert.Empty(t, out.String())
-		assert.Equal(t, "invalid value for flag --log-encoder: must be one of: json, console\n", errOut.String())
+		assert.Equal(t, "invalid value for flag --log-encoder: \"invalid\" must be one of: json, console\n", errOut.String())
 	})
 
 	t.Run("Not unique Annotations", func(t *testing.T) {
